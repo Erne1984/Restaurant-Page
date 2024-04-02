13b82efbb7464ccb4d589f15f607dd54fc1fc978
ernesto <ernestofamorim@gmail.com>
2024-04-02 02:06:06 -0300

    skeleton

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,12 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: "development",
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+    clean: true,
   },
   module: {
     rules: [
