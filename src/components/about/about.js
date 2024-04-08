3343ebae451baf0dded6b307d6f0f7ed13923113
ernesto <ernestofamorim@gmail.com>
2024-04-08 19:00:32 -0300

    working on about

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,5 +1,21 @@
 import "./about.css";
 
 const aboutDiv = document.createElement("div");
+const boxImg = document.createElement("img");
+const boxText = document.createElement("div");
+
+const titleAbout = document.createElement("h2");
+
+aboutDiv.classList.add("about")
+
+boxImg.classList.add("img-about")
+
+titleAbout.textContent = "Sobre nós"
+
+boxText.appendChild(titleAbout)
+
+aboutDiv.appendChild(boxImg);
+aboutDiv.appendChild(boxText);
+
 
 export default aboutDiv;
