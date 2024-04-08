1cf4908ada011fb4ef12e3832edb2014f645d027
Erne1917 <ernestofamorim@gmail.com>
2024-04-08 11:31:23 -0300

    ...

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,7 +1,20 @@
 import './home.css';
 
+const divHome = document.createElement("div");
 const title = document.createElement("h1");
+const content = document.createElement("p");
 
-title.innerHTML = "Soviet Cuisine";
+divHome.classList.add("home");
 
-export default title;
+title.innerHTML = "Soviet Cuisine ☭";
+
+content.textContent = `Explore as maravilhas gastronômicas da cozinha mais revolucionária de todas - Soviet Cuisine! 
+    Pratos capazes de alegrar os paladares e estomâgos de trabalhadores do mundo todo!
+    Trabalhadores do mundo, uni-vós!`;
+
+content.classList.add('text')
+
+divHome.appendChild(title);
+divHome.appendChild(content)
+
+export default divHome;
