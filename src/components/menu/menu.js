0f3759c08ba996181fb3572fcfffae76615537d9
ernesto <ernestofamorim@gmail.com>
2024-04-15 19:12:08 -0300

    ...

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,8 +1,28 @@
 import './menu.css';
 
+const containerMenu = document.createElement("div");
 const divMenu = document.createElement("div");
-const title = document.createElement("h1");
+
+const titleMenu = document.createElement("h1");
+
+const divCard = document.createElement("div");
 const text = document.createElement("p");
 
+// EDITING ELEMENTS
 
-export default divMenu;
+containerMenu.classList.add("container-menu");
+
+divMenu.classList.add("box-menu");
+
+titleMenu.textContent = "Menu";
+titleMenu.classList.add("title-menu");
+
+
+// APPENDING ELEMENTS
+
+containerMenu.appendChild(titleMenu);
+
+containerMenu.appendChild(divMenu);
+
+
+export default containerMenu;
